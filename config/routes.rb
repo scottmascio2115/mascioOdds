210be3d3b87033b4ddea3432f2ab602c2eb9e20f
Scott Mascio <scottmascio@gmail.com>
2015-09-20 17:11:40 -0400

    set games index to home

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
 
-  root 'welcome#index'
+  root 'games#index'
   resources :teams
   resources :games
 
